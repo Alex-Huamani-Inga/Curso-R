04eb2d36e106d11a2faea7c01cf2436bfe6c9d93
Alex-Huamani-Inga <ahuamanii@uni.pe>
2017-09-29 13:45:24 -0500

    solucion del archivo 5

---
+++ Practica1/Ejercicio5/Ejercicio5.R
@@ -0,0 +1,8 @@
+#Nombre: Alex Huamani Inga 20121383B
+#Respuesta pregunta 5
+tst4 < function(x){
+  if(x<-2) "muy negativo"
+  else if (x<1) "cercano a cero"
+  else if (x<3) "in [1, 3)"
+  else "large"
+}
